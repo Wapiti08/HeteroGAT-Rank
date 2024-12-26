23a2dca04f30beb43ddc594a75339d3e60284509
Wapiti08 <tzrzhuoran@gmail.com>
2024-12-26 14:35:37 +0000

    start to build personalized pagerank

---
+++ model/ppr.go
@@ -0,0 +1,16 @@
+/**
+ * @ Create Time: 2024-12-26 13:52:37
+ * @ Modified time: 2024-12-26 14:15:00
+ * @ Description: implement personalized pagerank with golang
+ */
+
+package model
+
+// Graph representation with edge weight
+
+// Node structure with value, type, and eco attributes
+
+// Edge Structure with source, target, value, and type
+
+// AddEdge adds a weighted edge to the graph
+
